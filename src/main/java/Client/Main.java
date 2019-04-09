982ec355b54fcf255088abab5700dec766586ed9
James Spagnola III <j-spags@hotmail.com>
2019-04-09 11:17:17 -0400

    Tweaks to gradle build and main to include functioning "Hello World" start

--- src/main/java/Client/Main.java
+++ src/main/java/Client/Main.java
@@ -10,12 +10,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Client.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Client.fxml"));
 
-        Scene scene = new Scene(root, 300, 275);
+        Scene scene = new Scene(root, 500, 475);
 
         stage.setTitle("FXML Welcome");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
