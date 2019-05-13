a6b89f029aed703fa9e9e0b6f4be95c2abae8a09
MikeA <mikey07anthony@gmail.com>
2019-05-12 20:16:38 -0400

    Some tests.

---
+++ api/src/test/java/packets/MessageAckPacketTest.java
@@ -0,0 +1,34 @@
+package packets;
+
+import utils.Constants;
+
+import java.nio.ByteBuffer;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class MessageAckPacketTest {
+
+    public void testSerializeWithSmallPacket() {
+        long input = 123456;
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(input);
+        byte[] bArray = buffer.array();
+        MessageAckPacket packet = new MessageAckPacket(input);
+        assertArrayEquals(bArray, packet.serialize());
+
+    }
+
+    public void testParseithSmallPacket() throws  InvalidPacketFormatException {
+        long input = 123456;
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(input);
+        byte[] bArray = buffer.array();
+        MessageAckPacket packet =  MessageAckPacket.parse(bArray);
+        assertEquals(packet,MessagePacket.parse(bArray));
+        assertEquals(Constants.OPCODE.MESSAGEACK, packet.getOperationCode());
+
+    }
+
+
+}
